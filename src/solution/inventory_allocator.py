104210a93bbc906430d23ff3788e963e17acafb4
Thejaswi Kampalli <thejaswi.kampalli@sjsu.edu>
2019-07-06 15:49:46 -0700

    error corrected

--- src/solution/inventory_allocator.py
+++ src/solution/inventory_allocator.py
@@ -8,21 +8,34 @@ class InventoryAllocator:
         :return: list of warehouses with their shipments
         """
         res = []
+        ordered_items_keys = ordered_items.keys()
         if InventoryAllocator.__is_cart_quantity_not_empty(ordered_items):
             for warehouse in warehouses:
                 inventory = warehouse['inventory']
 
-                matching_items = set(ordered_items.keys()).intersection(
+                # Get the common items among the ordered items and inventory items
+                matching_items = set(ordered_items_keys).intersection(
                     set(inventory.keys()))
 
+                # filter the items to eliminate already processed items and make sure inventory items don't have zero quantity
                 matching_items = {
                     i for i in matching_items if inventory[i] > 0 and ordered_items[i] > 0}
 
                 if matching_items:
+                    shipment_config = {}
                     for item in matching_items:
-                        ordered_items[item] -= inventory[item]
-                    res.append(warehouse)
+                        # calculating the shipment quantity for items
+                        shipment_config[item] = ordered_items[item] if ordered_items[item] < inventory[item] else \
+                            inventory[item]
+                        # if inventory stock is more than ordered stock update ordered items to 0
+                        # else update diff in ordered items
+                        ordered_items[item] = ordered_items[item] - inventory[item] if inventory[item] < ordered_items[
+                            item] else 0
 
+                    shipment = {warehouse['name']: shipment_config}
+                    res.append(shipment)
+
+            # check if the quantities are all less than or equal to zero
         return [] if InventoryAllocator.__is_cart_quantity_not_empty(ordered_items) else res
 
     @staticmethod
@@ -38,17 +51,10 @@ class InventoryAllocator:
 
 if __name__ == '__main__':
     allocator = InventoryAllocator()
-    print(allocator.cheapest_shipment_calculator({'apple': 10, 'orange': 7}, [
+    actual_result = allocator.cheapest_shipment_calculator({'apple': 10, 'orange': 7, 'banana': 4}, [
         {'name': 'abc', 'inventory': {'apple': 6, 'orange': 6}},
-        {'name': 'def', 'inventory': {'banana': 4}},
-        {'name': 'ghi', 'inventory': {'banana': 3, 'apple': 5}}
-    ]))
+        {'name': 'def', 'inventory': {'banana': 4, 'orange': 1}},
+        {'name': 'ghi', 'inventory': {'banana': 3, 'orange': 3, 'apple': 5}}
+    ])
 
-    print(allocator.cheapest_shipment_calculator(
-        {'apple': 1}, [{'name': 'owd', 'inventory': {'apple': 1}}]))
-
-    print(allocator.cheapest_shipment_calculator(
-        {'apple': 1}, [{'name': 'owd', 'inventory': {'apple': 0}}]))
-
-    print(allocator.cheapest_shipment_calculator({'apple': 10}, [{'name': 'owd', 'inventory': {'apple': 5}},
-                                                                 {'name': 'dm', 'inventory': {'apple': 5}}]))
+    print(actual_result)
--- src/solution/test_inventory_allocator.py
+++ src/solution/test_inventory_allocator.py
@@ -3,15 +3,17 @@ from inventory_allocator import InventoryAllocator
 
 def test_not_enough_inventory():
     allocator = InventoryAllocator()
-    actual_result = allocator.cheapest_shipment_calculator({'apple': 1}, [{'name': 'owd', 'inventory': {'apple': 0}}])
+    actual_result = allocator.cheapest_shipment_calculator(
+        {'apple': 5}, [{'name': 'owd', 'inventory': {'apple': 4}}])
     expected_result = []
     assert actual_result == expected_result
 
 
 def test_exact_match():
     allocator = InventoryAllocator()
-    actual_result = allocator.cheapest_shipment_calculator({'apple': 1}, [{'name': 'owd', 'inventory': {'apple': 1}}])
-    expected_result = [{'name': 'owd', 'inventory': {'apple': 1}}]
+    actual_result = allocator.cheapest_shipment_calculator(
+        {'apple': 4, 'banana': 5}, [{'name': 'owd', 'inventory': {'apple': 4, 'banana': 5}}])
+    expected_result = [{'owd': {'apple': 4, 'banana': 5}}]
     assert actual_result == expected_result
 
 
@@ -19,26 +21,27 @@ def test_multiple_match1():
     allocator = InventoryAllocator()
     actual_result = allocator.cheapest_shipment_calculator({'apple': 10}, [{'name': 'owd', 'inventory': {'apple': 5}},
                                                                            {'name': 'dm', 'inventory': {'apple': 5}}])
-    expected_result = [{'name': 'owd', 'inventory': {'apple': 5}},
-                       {'name': 'dm', 'inventory': {'apple': 5}}]
+    expected_result = [{'owd': {'apple': 5}},
+                       {'dm': {'apple': 5}}]
     assert actual_result == expected_result
 
 
-def test_multiple_match():
+def test_multiple_match2():
     allocator = InventoryAllocator()
-    actual_result = allocator.cheapest_shipment_calculator({'apple': 10, 'orange': 7, 'onion': 0}, [
+    actual_result = allocator.cheapest_shipment_calculator({'apple': 10, 'orange': 7, 'banana': 4}, [
         {'name': 'abc', 'inventory': {'apple': 6, 'orange': 6}},
-        {'name': 'def', 'inventory': {'banana': 4}},
+        {'name': 'def', 'inventory': {'banana': 4, 'orange': 1}},
         {'name': 'ghi', 'inventory': {'banana': 3, 'orange': 3, 'apple': 5}}
     ])
-    expected_result = [{'name': 'abc', 'inventory': {'apple': 6, 'orange': 6}},
-                       {'name': 'ghi', 'inventory': {'banana': 3, 'orange': 3, 'apple': 5}}]
+    expected_result = [{'abc': {'apple': 6, 'orange': 6}},
+                       {'def': {'banana': 4, 'orange': 1}},
+                       {'ghi': {'apple': 4}}]
     assert actual_result == expected_result
 
 
-def test_partial_match():
+def test_partial_match1():
     allocator = InventoryAllocator()
-    actual_result = allocator.cheapest_shipment_calculator({'apple': 10, 'orange': 7, 'onion': 0}, [
+    actual_result = allocator.cheapest_shipment_calculator({'apple': 10, 'orange': 7}, [
         {'name': 'abc', 'inventory': {'apple': 6, 'orange': 6}},
         {'name': 'def', 'inventory': {'banana': 4}},
         {'name': 'ghi', 'inventory': {'banana': 3, 'apple': 5}}])
@@ -46,7 +49,7 @@ def test_partial_match():
     assert actual_result == expected_result
 
 
-def test_partial_match1():
+def test_partial_match2():
     allocator = InventoryAllocator()
     actual_result = allocator.cheapest_shipment_calculator({'apple': 12, 'orange': 7, 'banana': 5}, [
         {'name': 'abc', 'inventory': {'apple': 6, 'orange': 6}},
